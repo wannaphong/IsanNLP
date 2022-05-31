b984d871663073c6628f21216613a27045dce7f2
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-05-31 10:40:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ requirements=['pythainlp']
 
 setup(
     name="IsanNLP",
-    version="0.1.dev1",
+    version="0.1.dev2",
     description="Isan Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="wannaphong",
-    author_email="wannaphong@kkumail.com",
+    package_data={
+        "isannlp": [
+            "corpus/*",
+        ]
+    },
+    author_email="wannaphong@yahoo.com",
     url="https://github.com/wannaphongcom/IsanNLP",
     packages=find_packages(),
     install_requires=requirements
